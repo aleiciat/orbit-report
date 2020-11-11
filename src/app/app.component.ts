a70c5403d1f05de82be7964dbe03883c105b5303
Aleicia Thomas <acelestialknight@outlook.com>
2020-11-11 18:22:40 -0500

    parts 1-5 complete, parts 6-8 need to be completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,47 @@ import { Satellite } from './satellite';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ls';
+  title = 'orbit-report';
   
   sourceList: Satellite[];
 
   constructor() {
-    this.sourceList = [
-       new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-       new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-       new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-       new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-       new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-    ];
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+ 
+          let fetchedSatellites = data.satellites;
+          let satellite;
+          for (let i = 0; i < fetchedSatellites.length; i++){
+          satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+          // if (fetchedSatellites[i].type.toUpperCase() === 'Space Debris'.toUpperCase()){
+          //   shouldShowWarning = true;
+          //   if(shouldShowWarning === true) {
+          //   //    should change the css for type- space debris 
+          //   }
+          this.sourceList.push(satellite);  
+        }
+ 
+       }.bind(this));
+    }.bind(this));
+ 
  }
+
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+}
 
 }
 
